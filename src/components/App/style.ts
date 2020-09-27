9f2aadfdaef531a9f6029ed4b3c6b99defb57820
Luciano Pierdona <lucianompj1@gmail.com>
2020-09-27 19:02:11 -0300

    basic template, created repository

---
+++ src/components/App/style.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+// #ffdbc5
+// #cf1b1b
+// #900d0d
+// #423144
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>
+        Tel<span>zir</span>
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
