6611aa44bc6d3be9e7127a3d0ad9736e389a4adf
Luciano Pierdona <lucianompj1@gmail.com>
2020-09-27 23:14:17 -0300

    created data

--- src/data.ts
+++ src/data.ts
@@ -1,4 +1,36 @@
 export const data = {
   ddd: ["Selecione", "011", "016", "018", "017"],
   plans: ["Selecione", "FaleMais 30", "FaleMais 60", "FaleMais 120"],
+  details: [
+    {
+      origin: "011",
+      destiny: "016",
+      pricePerMinute: 1.9,
+    },
+    {
+      origin: "011",
+      destiny: "017",
+      pricePerMinute: 1.7,
+    },
+    {
+      origin: "011",
+      destiny: "018",
+      pricePerMinute: 0.9,
+    },
+    {
+      origin: "016",
+      destiny: "011",
+      pricePerMinute: 2.9,
+    },
+    {
+      origin: "017",
+      destiny: "011",
+      pricePerMinute: 2.7,
+    },
+    {
+      origin: "018",
+      destiny: "011",
+      pricePerMinute: 1.9,
+    },
+  ],
 };
